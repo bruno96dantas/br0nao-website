9c94dc2c23830c0321468a1101720cd40de600aa
Bruno Dantas <bruno96dantas@gmail.com>
2025-01-21 15:42:49 -0300

    structure project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main className="p-8">
+        <h1 className="text-4xl font-bold">Bem-vindo ao meu portfólio!</h1>
+      </main>
+    </div>
+  );
+};
+
+export default App;
+
+
